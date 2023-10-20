23220dae90675d3d2ae8a54f6a9e4a7b9a15fd23
matteo-guenci <mguenci@gmail.com>
2023-10-20 17:00:03 +0200

    16:59

--- scripts/maintitle_animation.js
+++ scripts/maintitle_animation.js
@@ -1,28 +1,32 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 let interval = null;
 
-document.querySelector("#maintitle").onmouseover = event => {  
-  let iteration = 0;
-  
-  clearInterval(interval);
-  
-  interval = setInterval(() => {
-    event.target.innerText = event.target.innerText
-      .split("")
-      .map((letter, index) => {
-        if(index < iteration) {
-          return event.target.dataset.value[index];
-        }
-      
-        return letters[Math.floor(Math.random() * 26)]
-      })
-      .join("");
-    
-    if(iteration >= event.target.dataset.value.length){ 
-      clearInterval(interval);
+document.querySelector("#maintitle").onmouseover = event => {
+    if (loadedStyle === 'css/2030.css') {
+        let iteration = 0;
+
+        clearInterval(interval);
+
+        interval = setInterval(() => {
+            event.target.innerText = event.target.innerText
+                .split("")
+                .map((letter, index) => {
+                    if (index < iteration) {
+                        return event.target.dataset.value[index];
+                    }
+
+                    return letters[Math.floor(Math.random() * 26)]
+                })
+                .join("");
+
+            if (iteration >= event.target.dataset.value.length) {
+                clearInterval(interval);
+            }
+
+            iteration += 1 / 3;
+
+            // Apply animation specific to 'css/2030.css'.
+            // You can add your animation logic here.
+        }, 40);
     }
-    
-    iteration += 1 / 3;
-  }, 40);
 }
