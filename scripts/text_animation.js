c7ed1e40811b69ab1fa71cfec45ea8a8e36f7d83
matteo-guenci <mguenci@gmail.com>
2023-10-21 18:47:09 +0200

    18:47

--- scripts/text_animation.js
+++ scripts/text_animation.js
@@ -1,19 +1,23 @@
 // Funzione per simulare la scrittura del testo
 function typeText(element, text, index) {
-    if (loadedStyle === 'css/2030.css' && index < text.length) {
-        element.textContent += text.charAt(index);
-        setTimeout(function() {
-            typeText(element, text, index + 1);
-        }, 50);
+    if (loadedStyle === 'css/2030.css') {
+        if (index < text.length) {
+            element.textContent += text.charAt(index);
+            setTimeout(function() {
+                typeText(element, text, index + 1);
+            }, 50);
+        }
     }
 }
 
 // Seleziona tutti gli elementi con la classe "animated-text" e avvia l'effetto di scrittura
 document.addEventListener('DOMContentLoaded', function() {
-    var elements = document.querySelectorAll('.animated-text');
-    elements.forEach(function(element) {
-        var textToType = element.textContent;
-        element.textContent = ''; // Cancella il testo iniziale
-        typeText(element, textToType, 0);
-    });
+    if (loadedStyle === 'css/2030.css') {
+        var elements = document.querySelectorAll('.animated-text');
+        elements.forEach(function(element) {
+            var textToType = element.textContent;
+            element.textContent = ''; // Cancella il testo iniziale
+            typeText(element, textToType, 0);
+        });
+    }
 });
