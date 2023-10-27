2665ad34faeb27fb4fedbfa322ca6c3a6cdd367a
matteo-guenci <mguenci@gmail.com>
2023-10-27 16:03:23 +0200

    Update text_animation.js

--- scripts/text_animation.js
+++ scripts/text_animation.js
@@ -50,34 +50,75 @@
 //     }
 // });
 
-function typeText(element, text, index) {
-    if (index < text.length) {
-        element.textContent += text.charAt(index);
-        setTimeout(function() {
-            typeText(element, text, index + 1);
-        }, 50);
-    }
-}
+// function typeText(element, text, index) {
+//     if (index < text.length) {
+//         element.textContent += text.charAt(index);
+//         setTimeout(function() {
+//             typeText(element, text, index + 1);
+//         }, 50);
+//     }
+// }
 
-function checkStyleAndApplyChanges() {
-    if (loadedStyle === "css/2030.css") {
-        applyChanges();
-    }
-}
+// function checkStyleAndApplyChanges() {
+//     if (loadedStyle === "css/2030.css") {
+//         applyChanges();
+//     }
+// }
 
-function applyChanges() {
-    var elements = document.querySelectorAll('.animated-text');
-    elements.forEach(function(element) {
-        var textToType = element.textContent;
-        element.textContent = ''; // Clear the initial text
-        typeText(element, textToType, 0);
-    });
-}
+// function applyChanges() {
+//     var elements = document.querySelectorAll('.animated-text');
+//     elements.forEach(function(element) {
+//         var textToType = element.textContent;
+//         element.textContent = ''; // Clear the initial text
+//         typeText(element, textToType, 0);
+//     });
+// }
 
-document.addEventListener('DOMContentLoaded', function() {
-    checkStyleAndApplyChanges(); // Check the style when the page is loaded
+// document.addEventListener('DOMContentLoaded', function() {
+//     checkStyleAndApplyChanges(); // Check the style when the page is loaded
 
-    // Set an interval to check and apply changes every second
-    setInterval(checkStyleAndApplyChanges, 10000);
-});
+//     // Set an interval to check and apply changes every second
+//     setInterval(checkStyleAndApplyChanges, 1000);
+// });
 
+
+// var intervalId; // Variable to store the interval ID
+// var currentStyle = null; // Variable to keep track of the current style
+
+// function typeText(element, text, index) {
+//     if (index < text.length) {
+//         element.textContent += text.charAt(index);
+//         setTimeout(function() {
+//             typeText(element, text, index + 1);
+//         }, 50);
+//     }
+// }
+
+// function checkStyleAndApplyChanges() {
+//     if (loadedStyle === "css/2030.css") {
+//         if (currentStyle !== loadedStyle) {
+//             currentStyle = loadedStyle;
+//             clearInterval(intervalId); // Clear the interval when the style changes
+//             applyChanges();
+//         }
+//     } else {
+//         currentStyle = null;
+//         clearInterval(intervalId); // Clear the interval if style is not "css/2030.css"
+//     }
+// }
+
+// function applyChanges() {
+//     var elements = document.querySelectorAll('.animated-text');
+//     elements.forEach(function(element) {
+//         var textToType = element.textContent;
+//         element.textContent = ''; // Clear the initial text
+//         typeText(element, textToType, 0);
+//     });
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     checkStyleAndApplyChanges(); // Check the style when the page is loaded
+
+//     // Set an interval to check and apply changes every second
+//     intervalId = setInterval(checkStyleAndApplyChanges, 1000);
+// });
