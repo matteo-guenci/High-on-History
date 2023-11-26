57aa0aba217f1d696483f156d1aacce116ea938e
matteo-guenci <mguenci@gmail.com>
2023-11-26 14:50:41 +0100

    14:50

--- scripts/prova_access_pieces.js
+++
@@ -1,5 +0,0 @@
-// var specificDiv = document.getElementById("prova");
-// // specificDiv.style.position = "absolute";
-// specificDiv.style.top = "100px";
-// // specificDiv.style.left = "200px";
-// specificDiv.style.width = "300px";
