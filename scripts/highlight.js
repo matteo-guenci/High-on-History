2c10c0342a6b903caef28264474b47a26918f39f
matteo-guenci <mguenci@gmail.com>
2023-12-10 17:48:54 +0100

    17:48

---
+++ scripts/highlight.js
@@ -0,0 +1,81 @@
+var currentIndex = 0;
+var currentClassName = "";
+
+function highlightElement(className) {
+    var elements = document.querySelectorAll(className);
+
+    if (currentClassName !== className) {
+        currentIndex = 0;
+        if (currentClassName) {
+            var prevElements = document.querySelectorAll(currentClassName);
+            prevElements.forEach(function (element) {
+                element.classList.remove("highlight");
+            });
+        }
+    }
+
+    if (currentIndex > 0 && currentIndex !== elements.length) {
+        elements[currentIndex - 1].classList.remove("highlight");
+    }
+
+    if (currentIndex < elements.length) {
+        desiredFunction(elements[currentIndex])
+        function desiredFunction(elemento) {
+    // checking if element with this ID exists and if the element inside of accordion
+    //if (($('#'+desiredId).length) && ($('#'+desiredId).closest('.collapse').length)) {
+        if (elemento) {
+        // showing the closes parent '.collapse' of our ID
+        elemento.classList.add("highlight");
+        
+        // showing the closes parent '.collapse' of our ID
+        $(elemento).closest('.collapse').collapse('show');
+
+        // smooth animation to our ID
+        
+        currentIndex++;
+        setTimeout(function () {
+        // smooth animation to our ID
+        elemento.scrollIntoView({ behavior: "smooth", block: 'center',
+            inline: 'center' });
+
+        // behavior: 'auto',
+            // block: 'center',
+            // inline: 'center'
+        }, 400); // this interval is necessary for bootstrap to complete the accordion animation
+    }
+    
+    }
+    }
+    if (currentIndex == elements.length) {
+        elements[elements.length - 1].classList.remove("highlight");
+        currentIndex = 0;
+        desiredFunction(elements[currentIndex])
+        function desiredFunction(elemento) {
+    // checking if element with this ID exists and if the element inside of accordion
+    //if (($('#'+desiredId).length) && ($('#'+desiredId).closest('.collapse').length)) {
+        if (elemento) {
+        // showing the closes parent '.collapse' of our ID
+        elemento.classList.add("highlight");
+        
+        // showing the closes parent '.collapse' of our ID
+        $(elemento).closest('.collapse').collapse('show');
+
+        // smooth animation to our ID
+        
+        setTimeout(function () {
+        // smooth animation to our ID
+        elemento.scrollIntoView({ behavior: "smooth", block: 'center',
+            inline: 'center' });
+
+        // behavior: 'auto',
+            // block: 'center',
+            // inline: 'center'
+        }, 400); // this interval is necessary for bootstrap to complete the accordion animation
+    }
+    
+    }
+    }
+
+
+    currentClassName = className;
+}
