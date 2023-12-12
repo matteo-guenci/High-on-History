4ba52590b84ce6e17458431f06f7ffeba9c97511
matteo-guenci <mguenci@gmail.com>
2023-12-12 11:29:56 +0100

    11:29

--- scripts/circular_menu.js
+++ scripts/circular_menu.js
@@ -36,42 +36,4 @@ function onMouseUp() {
 toggleBtn.addEventListener("mousedown", onMouseDown);
 document.addEventListener("mousemove", onMouseMove);
 document.addEventListener("mouseup", onMouseUp);
-
-// function updateNavPosition() {
-//   const navStyle = window.getComputedStyle(nav);
-//   const navHeight = parseInt(navStyle.height);
-//   const windHeight = window.innerHeight;
-//   const scrollY = window.scrollY;
-//   const issuesSectionTop = issuesSection.offsetTop; // Ottieni la posizione della sezione delle issue
-
-//   // Calcola la posizione desiderata del menu
-//   let desiredTop = scrollY + margin;
-
-//   // Imposta i limiti superiore e inferiore
-//   const upperLimit = margin;
-//   const lowerLimit = windHeight - navHeight - margin;
-
-//   // Limita la posizione del menu nei limiti desiderati
-//   if (desiredTop < upperLimit) {
-//     desiredTop = upperLimit;
-//   } else if (desiredTop > lowerLimit) {
-//     desiredTop = lowerLimit;
-//   }
-
-//   // Aggiungi una condizione per far diventare fisso il menu quando raggiunge la sezione delle issue
-//   if (scrollY > issuesSectionTop - margin) {
-//     nav.classList.add("fixed");
-//   } else {
-//     nav.classList.remove("fixed");
-//   }
-
-//   // Imposta la nuova posizione del menu
-//   nav.style.top = `${desiredTop}px`;
-// }
-
-// window.addEventListener("scroll", updateNavPosition);
-// window.addEventListener("resize", updateNavPosition);
-
-// Inizializza la posizione del menu
-// updateNavPosition();
 });
